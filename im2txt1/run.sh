291a8fd39aa6158758ecfed1c7b202bd0a443909
duhuazhen <1013468842@qq.com>
2018-05-07 21:13:47 +0800

    Add files via upload

---
+++ im2txt1/run.sh
@@ -0,0 +1,15 @@
+# 模型文件夹或文件路径
+CHECKPOINT_PATH="/home/hehe/anaconda3/envs/tensorflow/im2txt/model/newmodel.ckpt-2000000"
+# 词汇文件
+VOCAB_FILE="/home/hehe/anaconda3/envs/tensorflow/im2txt/data/word_counts.txt"
+# 图片文件，多个图片用逗号隔开
+IMAGE_FILE="/home/hehe/anaconda3/envs/tensorflow/im2txt/data/images/3.jpg"
+
+# bazel编译
+bazel build -c opt //im2txt:run_inference
+
+# 用参数调用编译后的文件
+bazel-bin/im2txt/run_inference \
+  --checkpoint_path=${CHECKPOINT_PATH} \
+  --vocab_file=${VOCAB_FILE} \
+  --input_files=${IMAGE_FILE}
